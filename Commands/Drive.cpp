2fb44178000f074319725335a2ea4b61a5c299e9
Mike Rehberg <Git@TR182.com>
2014-01-25 16:30:33 -0500

    xBox 360 controller working via "GetRawAxis"

--- Commands/Drive.cpp
+++ Commands/Drive.cpp
@@ -8,6 +8,7 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in th future.
 #include "Drive.h"
+#include "../360Object.h"
 Drive::Drive() {
 	// Use requires() here to declare subsystem dependencies
 	// eg. requires(chassis);
@@ -22,8 +23,11 @@ void Drive::Initialize() {
 }
 // Called repeatedly when this Command is scheduled to run
 void Drive::Execute() {
-	float leftSpeed = Robot::oi->getLeftJoystick()->GetY();
-	float rightSpeed = Robot::oi->getRightJoystick()->GetY();
+//	float leftSpeed = Robot::oi->getLeftJoystick()->GetY();
+//	float rightSpeed = Robot::oi->getRightJoystick()->GetY();
+	
+	float leftSpeed = Robot::oi->getLeftJoystick()->GetRawAxis(LeftYAxis);		// xBox 360
+	float rightSpeed = Robot::oi->getLeftJoystick()->GetRawAxis(RightYAxis);	// xBox 360
 	
 //	Object360 * xbox = dynamic_cast<Object360*>(Robot::oi->getLeftJoystick());
 //	float leftSpeed  = xbox->GetLeftY() ;
